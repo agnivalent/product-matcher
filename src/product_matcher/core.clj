cb1b04f1900c5eadf980127665aab5f8476c96be
Eugene Hardbread <agnivalent@gmail.com>
2017-02-14 15:12:05 -0200

    now with manufacturer aliases

--- src/product_matcher/core.clj
+++ src/product_matcher/core.clj
@@ -32,13 +32,14 @@
 (def matching-functions
   [[(fn [l p] (any?
                str/starts-with?
-               (str (get-tr l :title))
+               (get-tr l :title)
                [(str/join " " [(get-tr p :manufacturer) (get-tr p :model)])
                 (str/join " " [(get-tr p :manufacturer) (get-tr p :family) (get-tr p :model)])]))
     0 1]
-   [(fn [l p] (.contains
-               (str (get-tr l :manufacturer))
-               (str (get-tr p :manufacturer))))
+   [(fn [l p] (any?
+               #(.contains %1 %2)
+               (get-tr l :manufacturer)
+               (into  [(get-tr p :manufacturer)] (manufacturer-aliases (get-tr p :manufacturer)))))
     0.5 1]
    ])
 
