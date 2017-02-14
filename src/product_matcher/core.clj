d1767a6faec7715aff8903cefcc95e68312ac903
Eugene Hardbread <agnivalent@gmail.com>
2017-02-14 11:14:04 -0200

    first solution: listing title starts with formatted product

--- src/product_matcher/core.clj
+++ src/product_matcher/core.clj
@@ -1,18 +1,41 @@
 (ns product-matcher.core
   (:gen-class)
-  (:require [clojure.data.json :as json]))
+  (:require [clojure.data.json :as json]
+            [clojure.string :as str]))
 
-;; lazy seq of all listings from listings.txt
-(defn listings [stream]
+(defn read-json-stream [stream]
+  "return lazy sequense of entities (as hashes) from JSON stream (should be one entity per line)"
   (map
    #(json/read-str % :key-fn keyword)
    (line-seq stream)))
 
-(with-open [stream (clojure.java.io/reader "resources/listings.txt")]
-  (do
-    (pprint (take 5 (listings stream)))))
+(defn load-all-from-file [file-name]
+  (with-open [stream (clojure.java.io/reader file-name)]
+    (doall
+     (read-json-stream stream))))
+
+(def listings (load-all-from-file "resources/listings.txt"))
+
+(def products (load-all-from-file "resources/products.txt"))
+
+(defn get-tr [hash keyword]
+  (str/trim (str/lower-case (get hash keyword ""))))
+
+(defn match-probability [listing product]
+  (if (str/starts-with?
+       (str (get-tr listing :title))
+       (str (get-tr product :manufacturer) " " (get-tr product :family) " " (get-tr product :model)))
+    1
+    0))
+
+()
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (time
+   (print "Matches: "
+          (count
+           (doall
+            (for [l listings p products
+                  :when (= 1 (match-probability l p))]
+              [l p]))))))
