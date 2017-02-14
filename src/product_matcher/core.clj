0f25de97aae86893cfff4eaf568833972c0f26ab
Eugene Hardbread <agnivalent@gmail.com>
2017-02-14 08:35:23 -0200

    added data files, json parsing

--- src/product_matcher/core.clj
+++ src/product_matcher/core.clj
@@ -1,5 +1,16 @@
 (ns product-matcher.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.json :as json]))
+
+;; lazy seq of all listings from listings.txt
+(defn listings [stream]
+  (map
+   #(json/read-str % :key-fn keyword)
+   (line-seq stream)))
+
+(with-open [stream (clojure.java.io/reader "resources/listings.txt")]
+  (do
+    (pprint (take 5 (listings stream)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
